bead78d1387561787d27c9a6519d021cec5610f1
med <theoutsider009@gmail.com>
2021-10-26 12:13:41 +0200

    Initial Commit

---
+++ VerticalTabiViewScrolling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VerticalTabiViewScrolling
+//
+//  Created by Adam Jemni on 10/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VerticalTabiViewScrolling/VerticalTabiViewScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalTabiViewScrollingApp.swift
+//  VerticalTabiViewScrolling
+//
+//  Created by Adam Jemni on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct VerticalTabiViewScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
